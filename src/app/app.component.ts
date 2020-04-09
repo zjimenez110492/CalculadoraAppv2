49e38c8d2e473bccaa84c14cbfa21a0d443009f9
zjimenez110492 <zjimenez@unicauca.edu.co>
2020-04-09 12:21:54 -0500

    Calculadora, botones de colores bootstrap, solución propia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CalculadoraApp';
+  valor1:number=0;
+  valor2:number=0;
+  resultado:number=0;
+  onSuma()
+  {
+   this.resultado=this.valor1+this.valor2;
+  }
+  onResta()
+  {
+    this.resultado=this.valor1-this.valor2;  
+  }
+  onMultiplicar()
+  {
+    this.resultado=this.valor1*this.valor2;  
+  }
+  onDividir()
+  {
+    this.resultado=this.valor1/this.valor2;  
+  }
 }
